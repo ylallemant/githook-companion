ba80643c8633f4974acde95f4e46e6b8eb3ed357
Yann Lallemant <yanndavid.lallemant@gea.com>
2025-02-17 15:45:35 +0100

    test: add tests for commit validation and formatting

---
+++ pkg/git/commit/commit_test.go
@@ -0,0 +1,215 @@
+package commit
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/ylallemant/githooks-butler/pkg/api"
+)
+
+func TestMessageType(t *testing.T) {
+	cases := []struct {
+		name               string
+		message            string
+		config             *api.Config
+		expectedCommitType string
+		expectedTypeFound  bool
+	}{
+		{
+			name:    "no matching commit type prefix",
+			message: "some changes",
+			config: &api.Config{
+				Commit: &api.Commit{
+					Types: []*api.CommitType{
+						{
+							Type: "feat",
+						},
+					},
+				},
+			},
+			expectedCommitType: "",
+			expectedTypeFound:  false,
+		},
+		{
+			name:    "matching commit type always lower case",
+			message: "feat:some changes",
+			config: &api.Config{
+				Commit: &api.Commit{
+					Types: []*api.CommitType{
+						{
+							Type: "FEAT",
+						},
+					},
+				},
+			},
+			expectedCommitType: "feat",
+			expectedTypeFound:  true,
+		},
+		{
+			name:    "matching commit type from multiple",
+			message: "Docs:some changes",
+			config: &api.Config{
+				Commit: &api.Commit{
+					Types: []*api.CommitType{
+						{
+							Type: "feat",
+						},
+						{
+							Type: "DOCS",
+						},
+						{
+							Type: "test",
+						},
+					},
+				},
+			},
+			expectedCommitType: "docs",
+			expectedTypeFound:  true,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			commitType, found := messageType(c.message, c.config)
+
+			assert.Equal(tt, c.expectedCommitType, commitType, "wrong result")
+			assert.Equal(tt, c.expectedTypeFound, found, "wrong result")
+		})
+	}
+}
+
+func TestEnsureFormat(t *testing.T) {
+	cases := []struct {
+		name       string
+		message    string
+		commitType string
+		expected   string
+	}{
+		{
+			name:       "add commit type prefix",
+			message:    "some changes",
+			commitType: "feat",
+			expected:   "feat: some changes",
+		},
+		{
+			name:       "dont change commit message case",
+			message:    "Some Heads-UP changes",
+			commitType: "feat",
+			expected:   "feat: Some Heads-UP changes",
+		},
+		{
+			name:       "ensure lower case commit type prefix",
+			message:    "Some Heads-UP changes",
+			commitType: "FEAT",
+			expected:   "feat: Some Heads-UP changes",
+		},
+		{
+			name:       "ensure lower case commit type prefix",
+			message:    "Some Heads-UP changes",
+			commitType: "FEAT",
+			expected:   "feat: Some Heads-UP changes",
+		},
+		{
+			name:       "ensure lower case commit type prefix on existing prefix",
+			message:    "FEAT : Some Heads-UP changes",
+			commitType: "FEAT",
+			expected:   "feat: Some Heads-UP changes",
+		},
+		{
+			name:       "only change first commit type name occurance",
+			message:    "FEAT : Some Heads-UP FEAT changes",
+			commitType: "FEAT",
+			expected:   "feat: Some Heads-UP FEAT changes",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			result := EnsureFormat(c.message, c.commitType)
+
+			assert.Equal(tt, c.expected, result, "wrong result")
+		})
+	}
+}
+
+func TestEnsureDictionaryValue(t *testing.T) {
+	cases := []struct {
+		name       string
+		message    string
+		dictionary *api.CommitTypeDictionary
+		expected   string
+	}{
+		{
+			name:    "replace first word",
+			message: "added some changes",
+			dictionary: &api.CommitTypeDictionary{
+				Name:  "add",
+				Value: "add",
+				Type:  "feat",
+				Synonyms: []string{
+					"adds",
+					"added",
+					"adding",
+					"new",
+				},
+			},
+			expected: "add some changes",
+		},
+		{
+			name:    "ignore upfront spaces",
+			message: "    added some changes",
+			dictionary: &api.CommitTypeDictionary{
+				Name:  "add",
+				Value: "add",
+				Type:  "feat",
+				Synonyms: []string{
+					"adds",
+					"added",
+					"adding",
+					"new",
+				},
+			},
+			expected: "add some changes",
+		},
+		{
+			name:    "ignore case",
+			message: "AdDs some changes",
+			dictionary: &api.CommitTypeDictionary{
+				Name:  "add",
+				Value: "add",
+				Type:  "feat",
+				Synonyms: []string{
+					"adds",
+					"added",
+					"adding",
+					"new",
+				},
+			},
+			expected: "add some changes",
+		},
+		{
+			name:    "ignore punctuation",
+			message: "AdDs: some changes",
+			dictionary: &api.CommitTypeDictionary{
+				Name:  "add",
+				Value: "add",
+				Type:  "feat",
+				Synonyms: []string{
+					"adds",
+					"added",
+					"adding",
+					"new",
+				},
+			},
+			expected: "add: some changes",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			result := EnsureDictionaryValue(c.message, c.dictionary)
+
+			assert.Equal(tt, c.expected, result, "wrong result")
+		})
+	}
+}
---
+++ pkg/git/commit/validation_test.go
@@ -0,0 +1,239 @@
+package commit
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/ylallemant/githooks-butler/pkg/api"
+)
+
+func TestIsMessageValid(t *testing.T) {
+	cases := []struct {
+		name       string
+		message    string
+		commitType string
+		expected   bool
+	}{
+		{
+			name:       "no matching commit type prefix",
+			message:    "some message",
+			commitType: "feat",
+			expected:   false,
+		},
+		{
+			name:       "matching commit type prefix",
+			message:    "feat: some message",
+			commitType: "feat",
+			expected:   true,
+		},
+		{
+			name:       "ignore prefix case",
+			message:    "FEAT: some message",
+			commitType: "feat",
+			expected:   true,
+		},
+		{
+			name:       "ignore missing colon",
+			message:    "FEAT some message",
+			commitType: "feat",
+			expected:   true,
+		},
+		{
+			name:       "ignore missing spaces",
+			message:    "FEAT:some message",
+			commitType: "feat",
+			expected:   true,
+		},
+		{
+			name:       "ignore to mutch space",
+			message:    "FEAT :   some message",
+			commitType: "feat",
+			expected:   true,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			validationExpression, err := regexp.Compile(
+				fmt.Sprintf(commitTypePrefixRegexpFmt, c.commitType),
+			)
+			assert.Nil(tt, err)
+
+			result := isMessageValid(c.message, validationExpression)
+
+			assert.Equal(tt, c.expected, result, "wrong result")
+		})
+	}
+}
+
+func TestValidationExpression(t *testing.T) {
+	cases := []struct {
+		name     string
+		config   *api.Config
+		expected string
+	}{
+		{
+			name: "no matching commit type prefix",
+			config: &api.Config{
+				Commit: &api.Commit{
+					Types: []*api.CommitType{
+						{
+							Type: "feat",
+						},
+					},
+				},
+			},
+			expected: "^(?i)feat\\s*:{0,1}\\s*",
+		},
+		{
+			name: "no types declared",
+			config: &api.Config{
+				Commit: &api.Commit{
+					Types: []*api.CommitType{},
+				},
+			},
+			expected: "^(?i)\\s*:{0,1}\\s*",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			result := validationExpression(c.config)
+
+			expectedRegexp, err := regexp.Compile(c.expected)
+			assert.Nil(tt, err)
+
+			assert.Equal(tt, expectedRegexp, result, "regular expressions do not match")
+		})
+	}
+}
+
+func TestValidate(t *testing.T) {
+	cases := []struct {
+		name                     string
+		message                  string
+		config                   *api.Config
+		expectedDictionaryFound  bool
+		expectedCommitType       string
+		expectedValidationResult bool
+	}{
+		{
+			name:    "no matching commit type prefix",
+			message: "some changes",
+			config: &api.Config{
+				Commit: &api.Commit{
+					Types: []*api.CommitType{
+						{
+							Type: "feat",
+						},
+					},
+				},
+			},
+			expectedDictionaryFound:  false,
+			expectedCommitType:       "",
+			expectedValidationResult: false,
+		},
+		{
+			name:    "matching commit type prefix",
+			message: "FeaT  :  some changes",
+			config: &api.Config{
+				Commit: &api.Commit{
+					Types: []*api.CommitType{
+						{
+							Type: "feat",
+						},
+					},
+					Dictionaries: []*api.CommitTypeDictionary{
+						{
+							Name:  "add",
+							Value: "add",
+							Type:  "feat",
+							Synonyms: []string{
+								"adds",
+								"added",
+								"adding",
+								"new",
+							},
+						},
+					},
+				},
+			},
+			expectedDictionaryFound:  false,
+			expectedCommitType:       "feat",
+			expectedValidationResult: true,
+		},
+		{
+			name:    "matching commit type with a dictionary",
+			message: "added some changes",
+			config: &api.Config{
+				Commit: &api.Commit{
+					Types: []*api.CommitType{
+						{
+							Type: "feat",
+						},
+					},
+					Dictionaries: []*api.CommitTypeDictionary{
+						{
+							Name:  "add",
+							Value: "add",
+							Type:  "feat",
+							Synonyms: []string{
+								"adds",
+								"added",
+								"adding",
+								"new",
+							},
+						},
+					},
+				},
+			},
+			expectedDictionaryFound:  true,
+			expectedCommitType:       "feat",
+			expectedValidationResult: true,
+		},
+		{
+			name:    "no matching commit type with a dictionary",
+			message: "added some changes",
+			config: &api.Config{
+				Commit: &api.Commit{
+					Types: []*api.CommitType{
+						{
+							Type: "feat",
+						},
+					},
+					Dictionaries: []*api.CommitTypeDictionary{
+						{
+							Name:  "nomatch",
+							Value: "no match",
+							Type:  "feat",
+							Synonyms: []string{
+								"nomatch",
+								"whatever",
+							},
+						},
+					},
+				},
+			},
+			expectedDictionaryFound:  false,
+			expectedCommitType:       "",
+			expectedValidationResult: false,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			valid, commitType, dictionary := Validate(c.message, c.config)
+
+			if c.expectedDictionaryFound {
+				assert.Equal(tt, c.config.Commit.Dictionaries[0], dictionary, "wrong result")
+			} else {
+				assert.Nil(tt, dictionary)
+			}
+
+			assert.Equal(tt, c.expectedCommitType, commitType, "wrong result")
+			assert.Equal(tt, c.expectedValidationResult, valid, "wrong result")
+		})
+	}
+}
