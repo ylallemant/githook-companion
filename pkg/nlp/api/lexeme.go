59576beebaa0c560b2d7a027084df6f2be4a89ca
Yann Lallemant <yann.lallemant@gmail.com>
2025-03-03 10:21:31 +0100

    add regexp lexemes

---
+++ pkg/nlp/api/lexeme.go
@@ -0,0 +1,33 @@
+package api
+
+import "regexp"
+
+type Lexeme struct {
+	Name    string
+	Regexps []*Regexp
+}
+
+type Regexp struct {
+	*regexp.Regexp
+}
+
+// UnmarshalText unmarshals json into a regexp.Regexp
+func (r *Regexp) UnmarshalText(b []byte) error {
+	regex, err := regexp.Compile(string(b))
+	if err != nil {
+		return err
+	}
+
+	r.Regexp = regex
+
+	return nil
+}
+
+// MarshalText marshals regexp.Regexp as string
+func (r *Regexp) MarshalText() ([]byte, error) {
+	if r.Regexp != nil {
+		return []byte(r.Regexp.String()), nil
+	}
+
+	return nil, nil
+}
