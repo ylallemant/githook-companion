6926bb9b289736afbdb7a405f65b1beec337805c
Yann Lallemant <yann.lallemant@gmail.com>
2025-03-07 08:17:15 +0100

    optimizing tests

--- pkg/nlp/fuzzy.go
+++ pkg/nlp/fuzzy.go
@@ -1,6 +1,7 @@
 package nlp
 
 import (
+	"fmt"
 	"math"
 	"strings"
 
@@ -8,13 +9,30 @@ import (
 )
 
 func calculateConfidence(word, entry string) float64 {
+
 	if word == entry {
 		return 1.0
 	}
 
-	distance := edlib.DamerauLevenshteinDistance(word, entry)
+	if len(word) > len(entry) {
+		return 0
+	}
 
-	return float64(distance)
+	position := positionDistance(word, entry)
+	characters := 1.1
+
+	if position > -1 {
+		characters = basicDistance(word, entry)
+	} else {
+		// no clear match
+		characters = charDistance(word, entry)
+	}
+
+	if characters > 1 {
+		return 0
+	}
+
+	return float64(1 - characters)
 }
 
 func distance(word, entry string) float64 {
@@ -27,7 +45,29 @@ func absoluteDistance(word, entry string) float64 {
 
 // not sure of this
 func basicDistance(word, entry string) float64 {
-	return maxDistance(word, entry) - distance(word, entry)
+	position := positionDistance(word, entry)
+	maximum := maxDistance(word, entry)
+	absolute := distance(word, entry)
+
+	if position < 0 {
+		return 1
+	}
+
+	lengthRatio := float64(absolute) / float64(maximum)
+	positionRatio := absolute
+
+	if absolute > 0 {
+		positionRatio = float64(position) / float64(absolute)
+	}
+
+	fmt.Println("  - position:           ", position)
+	fmt.Println("  - maximum:            ", maximum)
+	fmt.Println("  - absolute:           ", absolute)
+	fmt.Println("  - position / maxPos:  ", positionRatio)
+	fmt.Println("  - absolute / maximum:  ", float64(absolute)/float64(maximum))
+	fmt.Println("  - result:              ", lengthRatio-positionRatio)
+
+	return math.Abs(lengthRatio - positionRatio)
 }
 
 func maxDistance(word, entry string) float64 {
@@ -38,17 +78,129 @@ func maxDistance(word, entry string) float64 {
 	}
 }
 
-// the smaller the better
-func indexedDistance(word, entry string) float64 {
-	if len(word) > len(entry) {
-		return -1.0
-	}
-
+// positionDistance checks the position of the word in the entry
+// 0 is best, if not found -1 is returned
+func positionDistance(word, entry string) float64 {
 	index := strings.Index(entry, word)
 
-	if index > -1 {
-		return float64(index)
+	return float64(index)
+}
+
+// charDistance checks for the position of the individual characters
+// of the word in the entry.
+// the lower the returned value, the better
+func charDistance(word, entry string) float64 {
+	charPositions := make([][]int, 0)
+	maxPosition := len(entry) - 1
+
+	for _, rune := range word {
+		char := string(rune)
+		position := 0
+		lastPosition := 0
+		positions := make([]int, 0)
+
+		for position > -1 && position < maxPosition {
+
+			// position in truncated entry
+			position = strings.Index(entry[position:], char)
+
+			if position < 0 {
+				// char not found, stop search
+				break
+			}
+
+			// calcule real position in entry
+			position = lastPosition + position
+
+			positions = append(positions, position)
+
+			// calculate next start position
+			position = position + 1
+			lastPosition = position
+		}
+
+		charPositions = append(charPositions, positions)
 	}
 
-	return 0.0
+	// make a sums per index for:
+	//    - distance of scatered characters
+	//    - sum of all positions
+	//    - missing character count
+	//
+	// characters following each other in the right order
+	// do not increase the distance (perfect match)
+	//
+	// ex character positions :
+	// [[5 15] [6 16] [7 12 20] [8 13] [9]]
+	// ex sum by index:
+	// [
+	//   map[distance:5 missed:0 sum:35],
+	//   map[distance:27 missed:0 sum:56]
+	// ]
+	distances := make([]map[string]int, 0)
+
+	index := 0
+	maxLength := 1
+
+	for index < maxLength {
+		lastPosition := 0
+		totalDinstance := 0
+		totalSum := 0
+		totalMissed := 0
+		information := make(map[string]int)
+
+		for _, positions := range charPositions {
+			if index < len(positions) {
+				totalSum = totalSum + positions[index]
+
+				if positions[index] == lastPosition+1 {
+					lastPosition = positions[index]
+					continue
+				}
+
+				lastPosition = positions[index]
+				totalDinstance = totalDinstance + lastPosition
+			} else if index == 0 {
+				totalMissed = totalMissed + 1
+			}
+
+			if index == 0 && maxLength < len(positions) {
+				maxLength = len(positions)
+			}
+		}
+
+		information["distance"] = totalDinstance
+		information["missed"] = totalMissed
+		information["sum"] = totalSum
+
+		distances = append(distances, information)
+		index = index + 1
+	}
+
+	if len(distances) == 0 && distances[0]["distance"] == 0 {
+		// strings are equal
+		// return no distance as 0
+		return 0
+	}
+
+	// get a sum of the distances and sums
+	// example:
+	// [
+	//   map[distance:5 missed:0 sum:35],
+	//   map[distance:27 missed:0 sum:56]
+	// ]
+	//
+	// sum distance 32
+	// sum sum      91
+	// 32 / 91 = 0.3516483516483517
+	distance := 0
+	sum := 0
+
+	for _, information := range distances {
+		distance = distance + information["distance"] + information["missed"]
+		sum = sum + information["sum"]
+	}
+
+	// return distance / sum ratio
+	return float64(distance) / float64(sum)
 }
--- pkg/nlp/fuzzy_test.go
+++ pkg/nlp/fuzzy_test.go
@@ -13,45 +13,129 @@ var cases = []struct {
 	entry string
 }{
 	{
-		name:  "equal length",
-		word:  "equal",
-		entry: "equal",
-	},
-	{
-		name:  "word longer",
+		name:  "input longer than entry",
 		word:  "verylongword",
 		entry: "entry",
 	},
 	{
-		name:  "entry longer",
-		word:  "word",
-		entry: "verylongentry",
+		name:  "equal",
+		word:  "equal",
+		entry: "equal",
 	},
 	{
-		name:  "within entry",
-		word:  "thing",
-		entry: "somethingsinthemiddle",
+		name:  "short - shor",
+		word:  "shor",
+		entry: "short",
 	},
 	{
-		name:  "within spaced entry",
-		word:  "thing",
-		entry: "some things in the middle",
+		name:  "short - hort",
+		word:  "hort",
+		entry: "short",
 	},
 	{
-		name:  "inverted within spaced entry",
-		word:  "some things in the middle",
-		entry: "thing",
+		name:  "short - sho",
+		word:  "sho",
+		entry: "short",
+	},
+	{
+		name:  "short - ort",
+		word:  "ort",
+		entry: "short",
+	},
+	{
+		name:  "short - hor",
+		word:  "hor",
+		entry: "short",
+	},
+	{
+		name:  "something - somethin",
+		word:  "somethin",
+		entry: "something",
+	},
+	{
+		name:  "something - omething",
+		word:  "omething",
+		entry: "something",
+	},
+	{
+		name:  "something - somethi",
+		word:  "somethi",
+		entry: "something",
+	},
+	{
+		name:  "something - mething",
+		word:  "mething",
+		entry: "something",
+	},
+	{
+		name:  "something - ometh",
+		word:  "ometh",
+		entry: "something",
+	},
+	{
+		name:  "something - methi",
+		word:  "methi",
+		entry: "something",
+	},
+	{
+		name:  "something - ethin",
+		word:  "ethin",
+		entry: "something",
+	},
+	{
+		name:  "scattered - scatterd",
+		word:  "scatterd",
+		entry: "scattered",
+	},
+	{
+		name:  "scattered - sattered",
+		word:  "sattered",
+		entry: "scattered",
+	},
+	{
+		name:  "scattered - scatted",
+		word:  "scatted",
+		entry: "something",
+	},
+	{
+		name:  "scattered - sctered",
+		word:  "sctered",
+		entry: "something",
+	},
+	{
+		name:  "scattered - cater",
+		word:  "cater",
+		entry: "something",
+	},
+	{
+		name:  "scattered - ctter",
+		word:  "ctter",
+		entry: "something",
 	},
 }
 
 func Test_absoluteDistance(t *testing.T) {
 	expectations := map[string]float64{
-		"equal length":                 0,
-		"word longer":                  7,
-		"entry longer":                 9,
-		"within entry":                 16,
-		"within spaced entry":          20,
-		"inverted within spaced entry": 20,
+		"input longer than entry": 7,
+		"equal":                   0,
+		"short - shor":            1,
+		"short - hort":            1,
+		"short - sho":             2,
+		"short - ort":             2,
+		"short - hor":             2,
+		"something - somethin":    1,
+		"something - omething":    1,
+		"something - somethi":     2,
+		"something - mething":     2,
+		"something - ometh":       4,
+		"something - methi":       4,
+		"something - ethin":       4,
+		"scattered - scatterd":    1,
+		"scattered - sattered":    1,
+		"scattered - scatted":     2,
+		"scattered - sctered":     2,
+		"scattered - cater":       4,
+		"scattered - ctter":       4,
 	}
 
 	for _, c := range cases {
@@ -70,12 +154,26 @@ func Test_absoluteDistance(t *testing.T) {
 
 func Test_distance(t *testing.T) {
 	expectations := map[string]float64{
-		"equal length":                 0,
-		"word longer":                  9,
-		"entry longer":                 11,
-		"within entry":                 16,
-		"within spaced entry":          20,
-		"inverted within spaced entry": 20,
+		"input longer than entry": 9,
+		"equal":                   0,
+		"short - shor":            1,
+		"short - hort":            1,
+		"short - sho":             2,
+		"short - ort":             2,
+		"short - hor":             2,
+		"something - somethin":    1,
+		"something - omething":    1,
+		"something - somethi":     2,
+		"something - mething":     2,
+		"something - ometh":       4,
+		"something - methi":       4,
+		"something - ethin":       4,
+		"scattered - scatterd":    1,
+		"scattered - sattered":    1,
+		"scattered - scatted":     7,
+		"scattered - sctered":     7,
+		"scattered - cater":       8,
+		"scattered - ctter":       8,
 	}
 
 	for _, c := range cases {
@@ -94,12 +192,26 @@ func Test_distance(t *testing.T) {
 
 func Test_basicDistance(t *testing.T) {
 	expectations := map[string]float64{
-		"equal length":                 5,
-		"word longer":                  3,
-		"entry longer":                 2,
-		"within entry":                 5,
-		"within spaced entry":          5,
-		"inverted within spaced entry": 5,
+		"input longer than entry": 1,
+		"equal":                   0,
+		"short - shor":            0.2,
+		"short - hort":            0.8,
+		"short - sho":             0.4,
+		"short - ort":             0.6,
+		"short - hor":             0.6,
+		"something - somethin":    9,
+		"something - omething":    5,
+		"something - somethi":     2,
+		"something - mething":     20,
+		"something - ometh":       20,
+		"something - methi":       20,
+		"something - ethin":       20,
+		"scattered - scatterd":    1,
+		"scattered - sattered":    1,
+		"scattered - scatted":     1,
+		"scattered - sctered":     1,
+		"scattered - cater":       1,
+		"scattered - ctter":       1,
 	}
 
 	for _, c := range cases {
@@ -118,17 +230,145 @@ func Test_basicDistance(t *testing.T) {
 
 func Test_maxDistance(t *testing.T) {
 	expectations := map[string]float64{
-		"equal length":                 5,
-		"word longer":                  12,
-		"entry longer":                 13,
-		"within entry":                 21,
-		"within spaced entry":          25,
-		"inverted within spaced entry": 25,
+		"input longer than entry": 0,
+		"equal":                   7,
+		"short - shor":            9,
+		"short - hort":            5,
+		"short - sho":             16,
+		"short - ort":             20,
+		"short - hor":             20,
+		"something - somethin":    9,
+		"something - omething":    5,
+		"something - somethi":     2,
+		"something - mething":     20,
+		"something - ometh":       20,
+		"something - methi":       20,
+		"something - ethin":       20,
+		"scattered - scatterd":    9,
+		"scattered - sattered":    5,
+		"scattered - scatted":     16,
+		"scattered - sctered":     20,
+		"scattered - cater":       20,
+		"scattered - ctter":       20,
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(tt *testing.T) {
 			result := maxDistance(c.word, c.entry)
+
+			expected, ok := expectations[c.name]
+			assert.Equal(tt, true, ok, fmt.Sprintf("missing test result \"%s\"", c.name))
+
+			if ok {
+				assert.Equal(tt, expected, result, "wrong result")
+			}
+		})
+	}
+}
+
+func Test_positionDistance(t *testing.T) {
+	expectations := map[string]float64{
+		"input longer than entry": 0,
+		"equal":                   7,
+		"short - shor":            9,
+		"short - hort":            5,
+		"short - sho":             16,
+		"short - ort":             20,
+		"short - hor":             20,
+		"something - somethin":    9,
+		"something - omething":    5,
+		"something - somethi":     2,
+		"something - mething":     20,
+		"something - ometh":       20,
+		"something - methi":       20,
+		"something - ethin":       20,
+		"scattered - scatterd":    9,
+		"scattered - sattered":    5,
+		"scattered - scatted":     16,
+		"scattered - sctered":     20,
+		"scattered - cater":       20,
+		"scattered - ctter":       20,
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			result := positionDistance(c.word, c.entry)
+
+			expected, ok := expectations[c.name]
+			assert.Equal(tt, true, ok, fmt.Sprintf("missing test result \"%s\"", c.name))
+
+			if ok {
+				assert.Equal(tt, expected, result, "wrong result")
+			}
+		})
+	}
+}
+
+func Test_charDistance(t *testing.T) {
+	expectations := map[string]float64{
+		"input longer than entry": 0,
+		"equal":                   7,
+		"short - shor":            9,
+		"short - hort":            5,
+		"short - sho":             16,
+		"short - ort":             20,
+		"short - hor":             20,
+		"something - somethin":    9,
+		"something - omething":    5,
+		"something - somethi":     2,
+		"something - mething":     20,
+		"something - ometh":       20,
+		"something - methi":       20,
+		"something - ethin":       20,
+		"scattered - scatterd":    9,
+		"scattered - sattered":    5,
+		"scattered - scatted":     16,
+		"scattered - sctered":     20,
+		"scattered - cater":       20,
+		"scattered - ctter":       20,
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			result := charDistance(c.word, c.entry)
+
+			expected, ok := expectations[c.name]
+			assert.Equal(tt, true, ok, fmt.Sprintf("missing test result \"%s\"", c.name))
+
+			if ok {
+				assert.Equal(tt, expected, result, "wrong result")
+			}
+		})
+	}
+}
+
+func Test_calculateConfidence(t *testing.T) {
+	expectations := map[string]float64{
+		"input longer than entry": 0,
+		"equal":                   7,
+		"short - shor":            9,
+		"short - hort":            5,
+		"short - sho":             16,
+		"short - ort":             20,
+		"short - hor":             20,
+		"something - somethin":    9,
+		"something - omething":    5,
+		"something - somethi":     2,
+		"something - mething":     20,
+		"something - ometh":       20,
+		"something - methi":       20,
+		"something - ethin":       20,
+		"scattered - scatterd":    9,
+		"scattered - sattered":    5,
+		"scattered - scatted":     16,
+		"scattered - sctered":     20,
+		"scattered - cater":       20,
+		"scattered - ctter":       20,
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			result := calculateConfidence(c.word, c.entry)
 
 			expected, ok := expectations[c.name]
 			assert.Equal(tt, true, ok, fmt.Sprintf("missing test result \"%s\"", c.name))
