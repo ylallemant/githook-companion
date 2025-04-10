c2ecc08e7ccd530a181d616e53482f10579bd0db
Yann Lallemant <yann.lallemant@gmail.com>
2025-04-10 12:13:39 +0200

    FEAT: add post-commit and pre-push githooks

--- pkg/git/hook/hooks/index.go
+++ pkg/git/hook/hooks/index.go
@@ -3,12 +3,16 @@ package hooks
 import "embed"
 
 var (
-	//go:embed prepare-commit-msg
 	//go:embed pre-commit
+	//go:embed prepare-commit-msg
+	//go:embed post-commit
+	//go:embed pre-push
 	CommonHooks embed.FS
 
 	Hooks = []string{
-		"prepare-commit-msg",
 		"pre-commit",
+		"prepare-commit-msg",
+		"post-commit",
+		"pre-push",
 	}
 )
