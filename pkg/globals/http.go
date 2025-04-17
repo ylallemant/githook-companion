2d0a846e323bed5e6504fb17737282817216e7e8
Yann Lallemant <yann.lallemant@gmail.com>
2025-04-17 16:23:02 +0200

    PERF: limit sync timeout to 5 seconds

--- pkg/globals/http.go
+++ pkg/globals/http.go
@@ -1,24 +1,10 @@
 package globals
 
 import (
-	"net"
 	"net/http"
 	"time"
 )
 
 var DefaultApiClient = &http.Client{
-	// Transport: transport(),
-}
-
-func transport() *http.Transport {
-	return &http.Transport{
-		DialTLSContext: dialer().DialContext,
-		DialContext:    dialer().DialContext,
-	}
-}
-
-func dialer() *net.Dialer {
-	return &net.Dialer{
-		Timeout: 3 * time.Second,
-	}
+	Timeout: 5 * time.Second,
 }
